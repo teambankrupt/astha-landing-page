8b6ca167357f44b8bc0747b45cae6f476cf04b88
Sayem Hossain <sayemkcn@gmail.com>
2021-03-24 14:29:41 +0600

    [Add] countdown page

---
+++ count.js
@@ -0,0 +1,33 @@
+// C O U N T D O W N //
+
+// Set the date we're counting down to
+var countDownDate = new Date("Jun 1, 2021 00:00:01").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    var daysStr = days > 1 ? " DAYS " : " DAY ";
+    var hoursStr = days > 1 ? " HOURS " : " HOUR ";
+    var minsStr = days > 1 ? " MINUTES     " : " MINUTE    ";
+    document.getElementById("demo").innerHTML = days + daysStr + hours + hoursStr
+        + minutes + minsStr + seconds + " SECONDS   ";
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
