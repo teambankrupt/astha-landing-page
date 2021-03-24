bfbb7158c502c3a15d215266c0ccbad35c1dd89b
Sayem Hossain <sayemkcn@gmail.com>
2021-03-24 14:39:05 +0600

    [Update] landing page

--- count.js
+++ count.js
@@ -19,11 +19,14 @@ var x = setInterval(function () {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Output the result in an element with id="demo"
-    var daysStr = days > 1 ? " DAYS " : " DAY ";
-    var hoursStr = days > 1 ? " HOURS " : " HOUR ";
-    var minsStr = days > 1 ? " MINUTES     " : " MINUTE    ";
+    var daysStr = days > 1 ? " <small>days</small> " : " <small>day</small> ";
+    var hoursStr = days > 1 ? " <small>hours</small> " : " <small>hour</small> ";
+    var minsStr = days > 1 ? " <small>minutes</small>     " : " <small>minute</small>     ";
+    // var daysStr = days > 1 ? " D " : " D ";
+    // var hoursStr = days > 1 ? " H " : " H ";
+    // var minsStr = days > 1 ? " M     " : " M    ";
     document.getElementById("demo").innerHTML = days + daysStr + hours + hoursStr
-        + minutes + minsStr + seconds + " SECONDS   ";
+        + minutes + minsStr + seconds + " <small>seconds</small>   ";
 
     // If the count down is over, write some text 
     if (distance < 0) {
